415d1a8be0fc2f74d740677ac2bb3e354ad8ae90
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-05-11 15:17:55 -0300

    feat: implementação de utilitário para formatação de data e valor

---
+++ frontend/src/utils/fomatters.js
@@ -0,0 +1,18 @@
+//Fomatar Data DD/MM/YYYY
+export const formatarData = (data) => {
+    return new Date(data).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+    });
+};
+
+//Formatar valor BRL(R$)
+export const formatarMoeda = (valor) => {
+    return new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    }).format(valor);
+};
