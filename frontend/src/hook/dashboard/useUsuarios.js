5f689e6b3689f83e84057a1a0f14637a37ea3671
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-05-11 16:02:55 -0300

    feat: implementação de hook para usuário

--- frontend/src/hook/dashboard/useUsuarios.js
+++ frontend/src/hook/dashboard/useUsuarios.js
@@ -8,3 +8,24 @@ const INITIAL_FORM_DATA = {
     nivelAcesso: 'admin',
     status: 'ativo'
 };
+
+const useUsuarios = (mostrarMensagem) => {
+    const [usuarios, setUsuarios] = useState([]);
+    const [openDialog, setOpenDialog] = useState(false);
+    const [selectedUsuario, setSelectedUsuario] = useState(null);
+    const [formData. setFormData] = useState(INITIAL_FORM_DATA);
+
+    useEffect(() => {
+        carregarUsuarios();
+    }, []);
+
+    const carregarUsuarios = async () => {
+        try{
+            const data = await dashboardService.usuarios
+        }catch(error){
+
+        }
+    }
+};
+
+export default useUsuarios;
