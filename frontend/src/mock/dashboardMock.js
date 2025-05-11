80d5588ad3c2cb3421fbbda2ebadf289b36325fd
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-05-11 14:50:06 -0300

    feat: implementação de dados mockados

---
+++ frontend/src/mock/dashboardMock.js
@@ -0,0 +1,24 @@
+export const mockEventos = [
+    {
+    id: 1,
+    titulo: 'Show de Rock',
+    data: new Date().toISOString(),
+    local: 'Estádio Municipal',
+    descricao: 'Grande show com banda de rock.',
+    preco: 100,
+    quantidadeIngressos: 500,
+    categoria: 'Música',
+    status: 'ativo'
+  },
+  {
+    id: 2,
+    titulo: 'Feira de Tecnologia',
+    data: new Date().toISOString(),
+    local: 'Centro de Convenções',
+    descricao: 'Exposição de startups e gadgets.',
+    preco: 50,
+    quantidadeIngressos: 300,
+    categoria: 'Tecnologia',
+    status: 'rascunho'
+  }
+];
