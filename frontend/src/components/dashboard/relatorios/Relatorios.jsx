28288c1b1d28fa289717127c434d19a396f5ee62
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-05-13 13:09:38 -0300

    feat: implentação do componente para Dashboard guia de Relatórios

---
+++ frontend/src/components/dashboard/relatorios/Relatorios.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {
+  Box, Typography, Grid, Paper, TextField, MenuItem,
+  Button, Table, TableBody, TableCell, TableContainer, TableHead,
+  TableRow,Card,CardContent
+} from '@mui/material';
+import { 
+    BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip,
+    Legend, ResponsiveContainer, PieChart, Pie, Cell
+} from 'recharts';
+import useRelatorios from '../../../hook/dashboard/useRelatorios';
