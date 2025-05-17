3d062ebc0504687fc952ddee73a53451bc178ebf
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-05-17 12:11:24 -0300

    feat: implementação  de rota para upload de imagens, post e put

--- backend/src/routes/evento.routes.js
+++ backend/src/routes/evento.routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router(); 
-
 const eventoController = require('../controllers/eventoController');
 const {validarCriacaoEvento, validarConsultaEventos, validarAtualizacaoEvento} = require('../validators/eventoValidators')
 const {autenticar} = require('../middlewares/auth');
+const { upload } = require('../middlewares/upload');
 
 // Rotas públicas
 router.get('/', validarConsultaEventos, eventoController.listarEventos); // Listar eventos
@@ -11,8 +11,8 @@ router.get('/:id', eventoController.buscarEventoPorId);
 
 
 // Rotas protegidas
-router.post('/', autenticar, validarCriacaoEvento , eventoController.criarEvento); // Criar evento
-router.put('/:id', autenticar, validarAtualizacaoEvento, eventoController.atualizarEventoPeloId)
+router.post('/', autenticar, validarCriacaoEvento, upload, eventoController.criarEvento); // Criar evento
+router.put('/:id', autenticar, validarAtualizacaoEvento, upload, eventoController.atualizarEventoPeloId)
 router.delete('/:id', autenticar, eventoController.deletarEventoPeloId);
 
 module.exports = router;
