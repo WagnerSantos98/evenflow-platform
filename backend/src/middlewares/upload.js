cc554ede9329aa652242505086ca730ebebf7491
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-05-20 16:23:57 -0300

    feat: implementação de método para excluir avatar quando atualizar

--- backend/src/middlewares/upload.js
+++ backend/src/middlewares/upload.js
@@ -3,22 +3,33 @@ const multer = require('multer');
 const storage = multer.memoryStorage();
 
 function fileFilter(req, file, cb) {
-    if(file.mimetype.startsWith('image/')) {
+    if (file.mimetype.startsWith('image/')) {
         cb(null, true);
     } else {
         cb(new Error('Apenas arquivos de imagem são permitidos!'), false);
     }
 }
 
-const upload = multer({
-    storage: storage,
-    fileFilter: fileFilter,
+const uploadUsuario = multer({
+    storage,
+    fileFilter,
+    limits: {
+        fileSize: 5 * 1024 * 1024 // 5MB
+    }
+}).single('foto'); //campo do upload de avatar/foto
+
+const uploadEvento = multer({
+    storage,
+    fileFilter,
     limits: {
         fileSize: 5 * 1024 * 1024 // 5MB
     }
 }).fields([
-    { name: 'capa', maxCount: 1 },    // Campo para a imagem de capa
-    { name: 'galeria', maxCount: 10 } // Campo para as imagens da galeria
+    { name: 'capa', maxCount: 1 },
+    { name: 'galeria', maxCount: 10 }
 ]);
 
-module.exports = { upload };
+module.exports = {
+    uploadUsuario,
+    uploadEvento
+};
