c33a94ea01f4bb5b84bf4e8c9b0bc58014e8ef07
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-05-20 16:26:46 -0300

    fix: correção das rotas e spread de validação

--- backend/src/routes/evento.routes.js
+++ backend/src/routes/evento.routes.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const eventoController = require('../controllers/eventoController');
 const {validarCriacaoEvento, validarConsultaEventos, validarAtualizacaoEvento} = require('../validators/eventoValidators')
 const {autenticar} = require('../middlewares/auth');
-const { upload } = require('../middlewares/upload');
+const { uploadEvento } = require('../middlewares/upload');
 
 // Rotas públicas
-router.get('/', validarConsultaEventos, eventoController.listarEventos); // Listar eventos
+router.get('/', ...validarConsultaEventos, eventoController.listarEventos); // Listar eventos
 router.get('/:id', eventoController.buscarEventoPorId); 
 
 
 // Rotas protegidas
-router.post('/', autenticar, upload, validarCriacaoEvento, eventoController.criarEvento); // Criar evento
-router.put('/:id', autenticar, validarAtualizacaoEvento, eventoController.atualizarEventoPeloId)
+router.post('/', autenticar, uploadEvento, ...validarCriacaoEvento, eventoController.criarEvento); // Criar evento
+router.put('/:id', autenticar, uploadEvento, ...validarAtualizacaoEvento, eventoController.atualizarEventoPeloId)
 router.delete('/:id', autenticar, eventoController.deletarEventoPeloId);
 
 module.exports = router;
