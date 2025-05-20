7e138e869e3d15742f4c4a15a68ffbc068f9a74b
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-05-20 16:26:52 -0300

    fix: correção das rotas e spread de validação

--- backend/src/routes/usuario.routes.js
+++ backend/src/routes/usuario.routes.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
 const { autenticar } = require('../middlewares/auth');
 const { validarCadastroUsuario, validarAtualizacaoUsuario, validarConsultaUsuarios } = require('../validators/usuarioValidators');
-const { upload } = require('../middlewares/upload');
+const { uploadUsuario } = require('../middlewares/upload');
 
 //Rotas públicas
-router.post('/', validarCadastroUsuario, upload, usuarioController.cadastrarUsuario);
-router.get('/', validarConsultaUsuarios, usuarioController.listarUsuario);
+router.post('/', uploadUsuario, ...validarCadastroUsuario, usuarioController.cadastrarUsuario);
+router.get('/', ...validarConsultaUsuarios, usuarioController.listarUsuario);
 router.get('/:id', usuarioController.buscarUsuario);
 
 //Rotas protegidas
-router.put('/:id', autenticar, validarAtualizacaoUsuario, upload, usuarioController.atualizarUsuario);
+router.put('/:id', autenticar, uploadUsuario, ...validarAtualizacaoUsuario, usuarioController.atualizarUsuario);
 router.delete('/:id', autenticar, usuarioController.deletarUsuario);
 
 module.exports = router;
