8de4337c988b530d11afa279f4ddae409fbcc820
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-05-23 10:48:17 -0300

    fix: remoção de sweet alert

--- frontend/src/utils/alertService.js
+++
@@ -1,9 +0,0 @@
-import { baseAlert } from './alerts/baseAlert';
-import { usuariosAlert } from './alerts/alerts';
-
-const alertService = {
-    base: baseAlert,
-    usuarios: usuariosAlert
-};
-
-export default alertService;
